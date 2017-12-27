c4d9c60b7bc87a5ed1284675901d954c7a7749d4
marcusvolz <marcus.volz@outlook.com>
2017-12-27 21:38:43 +1100

    added elevations vis

---
+++ R/plot_elevations.R
@@ -0,0 +1,32 @@
+#' Creates a plot of elevation profiles as small multiples
+#'
+#' Creates a plot of elevation profiles as small multiples
+#' @param data A data frame output from process_data()
+#' @keywords
+#' @export
+#' @examples
+#' plot_elevations()
+
+plot_elevations <- function(data) {
+  # Compute total distance for each activity
+  dist <- data %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise(dist = max(cumdist))
+
+  # Normalise distance
+  data <- data %>%
+    dplyr::left_join(dist, by = "id") %>%
+    dplyr::mutate(dist_scaled = cumdist / dist) %>%
+    dplyr::arrange(id, cumdist)
+
+  # Create plot
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_line(ggplot2::aes(dist_scaled, ele, group = id), data, alpha = 0.75) +
+    ggplot2::facet_wrap(~id) +
+    ggplot2::theme_void() +
+    ggplot2::theme(panel.spacing = ggplot2::unit(0, "lines"),
+                   strip.background = ggplot2::element_blank(),
+                   strip.text = ggplot2::element_blank(),
+                   plot.margin = ggplot2::unit(rep(1, 4), "cm"))
+  p
+}
