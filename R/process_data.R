0bd133569ef0aac3a6e1c1a77f7e410b0877fef1
marcusvolz <marcus.volz@outlook.com>
2018-01-09 15:11:52 +1100

    fixed lat/lon reversal in sp::spDists (Issue #9)

--- R/process_data.R
+++ R/process_data.R
@@ -29,7 +29,7 @@ process_data <- function(path) {
 
     # Put everything in a data frame
     result <- data.frame(lat = lat, lon = lon, ele = ele, time = time) %>%
-      dplyr::mutate(dist_to_prev = c(0, sp::spDists(x = as.matrix(.[, 1:2]), longlat = TRUE, segments = TRUE)),
+      dplyr::mutate(dist_to_prev = c(0, sp::spDists(x = as.matrix(.[, c("lon", "lat")]), longlat = TRUE, segments = TRUE)),
              cumdist = cumsum(dist_to_prev),
              time = as.POSIXct(.$time, format = "%Y-%m-%dT%H:%M:%OS")) %>%
       dplyr::mutate(time_diff_to_prev = as.numeric(difftime(time, dplyr::lag(time, default = .$time[1]))),
