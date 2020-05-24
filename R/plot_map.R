444cbf9d077d03b3c78edd5fc32be59a609b932a
mihagazvoda <miha.gazvoda1@gmail.com>
2020-05-24 20:08:26 +0200

    refactor

--- R/plot_map.R
+++ R/plot_map.R
@@ -2,24 +2,27 @@
 #'
 #' Plots activities superimposed on a map
 #' @param data A data frame output from process_data()
-#' @param lon_min Minimum longitude
-#' @param lon_max Maximum longitude
-#' @param lat_min Minimum latitude
-#' @param lat_max Maximum latitude
-#' @keywords
+#' @param lon_min Minimum longitude (optional)
+#' @param lon_max maximum longitude (optional)
+#' @param lat_min Minimum latitude (optional)
+#' @param lat_max Maximum latitude (optional)
+#'
+#' @return A heat map of activities
 #' @export
+#'
 #' @examples
-#' plot_map()
-
 plot_map <- function(data, lon_min = -180, lon_max = 180, lat_min = -90, lat_max = 90) {
-  # Create plot
-  p <- ggplot2::ggplot() +
-    ggplot2::geom_path(ggplot2::aes(lon, lat, group = id),
-              data %>% dplyr::filter(lon > lon_min, lon < lon_max, lat > lat_min, lat < lat_max),
-              alpha = 0.3, size = 0.3, lineend = "round") +
-    ggplot2::coord_map() +
-    ggplot2::theme_void()
-
-  # Return plot
-  p
+  data %>%
+    filter(
+      between(lon, lon_min, lon_max),
+      between(lat, lat_min, lat_max)
+    ) %>%
+    ggplot(aes(lon, lat, group = id)) +
+    geom_path(
+      alpha = 0.3,
+      size = 0.3,
+      lineend = "round"
+    ) +
+    coord_map() +
+    theme_void()
 }
