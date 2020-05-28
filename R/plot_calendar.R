14cc7fd3347c6b98c3b52aa63984c914bb88261e
mihagazvoda <miha.gazvoda1@gmail.com>
2020-05-28 18:39:37 +0200

    add unit parameter

--- R/plot_calendar.R
+++ R/plot_calendar.R
@@ -1,25 +1,34 @@
 #' Display Strava activities as calendar heat map
 #'
 #' @param data A data frame output from process_data()
+#' @param unit A string ("distance" or "time") defining which unit to display
 #'
 #' @return A plot displaying calendar heat map
 #' @export
 #'
 #' @examples
-plot_calendar <- function(data) {
-  distance_per_date <- data %>%
+plot_calendar <- function(data, unit = "distance") {
+  if (!(unit %in% c("distance", "time"))) {
+    stop("This unit doesn't exist! Use 'time' or 'distance' instead!")
+  }
+
+  unit_per_date <- data %>%
     mutate(date = lubridate::date(time)) %>%
     group_by(date) %>%
-    summarise(dist = sum(dist_to_prev)) %>%
+    summarise(
+      distance = sum(dist_to_prev),
+      time = sum(time_diff_to_prev) / 3600,
+      unit = !!sym(unit)
+    ) %>%
     ungroup() %>%
     tidyr::complete(
       date = seq(min(date), max(date), by = "1 day"),
       fill = list(dist = NA)
-      )
+    )
 
   ggTimeSeries::ggplot_calendar_heatmap(
-    distance_per_date,
-    "date", "dist",
+    unit_per_date,
+    "date", "unit",
     dayBorderSize = 0.5,
     dayBorderColour = "white",
     monthBorderSize = 0.75,
@@ -29,11 +38,11 @@ plot_calendar <- function(data) {
     xlab(NULL) +
     ylab(NULL) +
     scale_fill_continuous(
-      name = "km",
-      low = "#DAE580",
-      high = "#236327",
-      na.value = "#EFEDE0"
-      ) +
+      name = if (unit == "distance") "km" else "hr",
+      low = "#FFE6D6",
+      high = "#FE5502",
+      na.value = "#f9f8f8"
+    ) +
     facet_wrap(~Year, ncol = 1) +
     ggthemes::theme_tufte() +
     theme(strip.text = element_text(), axis.ticks = element_blank(), legend.position = "bottom")
