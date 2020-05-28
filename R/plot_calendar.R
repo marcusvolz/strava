bb753a8f1ed115d85ad6c6f13f542c995e545ef9
mihagazvoda <miha.gazvoda1@gmail.com>
2020-05-28 08:01:12 +0200

    add missing dates

--- R/plot_calendar.R
+++ R/plot_calendar.R
@@ -10,7 +10,12 @@ plot_calendar <- function(data) {
   distance_per_date <- data %>%
     mutate(date = lubridate::date(time)) %>%
     group_by(date) %>%
-    summarise(dist = sum(dist_to_prev))
+    summarise(dist = sum(dist_to_prev)) %>%
+    ungroup() %>%
+    tidyr::complete(
+      date = seq(min(date), max(date), by = "1 day"),
+      fill = list(dist = NA)
+      )
 
   ggTimeSeries::ggplot_calendar_heatmap(
     distance_per_date,
@@ -28,7 +33,7 @@ plot_calendar <- function(data) {
       low = "#DAE580",
       high = "#236327",
       na.value = "#EFEDE0"
-      ) + # trans = "log" if needed
+      ) +
     facet_wrap(~Year, ncol = 1) +
     ggthemes::theme_tufte() +
     theme(strip.text = element_text(), axis.ticks = element_blank(), legend.position = "bottom")
