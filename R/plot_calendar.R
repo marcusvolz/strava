8440612fa946b1644dcd0635745765ca32cc609c
Andrew Collier <andrew@fathomdata.dev>
2021-10-10 06:20:02 +0100

    feat: Add example to satisfy checks
    Currently getting this warning:
    
    * checking for code which exercises the package ... WARNING
    Warning: No examples, no tests, no vignettes

--- R/plot_calendar.R
+++ R/plot_calendar.R
@@ -7,6 +7,8 @@
 #' @export
 #'
 #' @examples
+#' running <- process_data(system.file("gpx/running", package = "strava"))
+#' plot_calendar(running)
 plot_calendar <- function(data, unit = "distance") {
   if (!(unit %in% c("distance", "time"))) {
     stop("This unit doesn't exist! Use 'time' or 'distance' instead!")
